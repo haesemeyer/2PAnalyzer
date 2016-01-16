e159c5af17b2475359fb74ea619b28c1b71b0deb
Martin Haesemeyer <m.haesemeyer@gmail.com>
2016-01-16 11:33:11 -0500

    Added code to request and dispose memory for the image data.

--- PluginAPI/ImageStack.cs
+++ PluginAPI/ImageStack.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,15 +11,52 @@ namespace TwoPAnalyzer.PluginAPI
     public abstract class ImageStack : IDisposable
     {
         #region Class Members
+
+        /// <summary>
+        /// Pointer to the image data
+        /// </summary>
+        protected IntPtr _imageData = IntPtr.Zero;
+
+        /// <summary>
+        /// Indicates whether we own and should free
+        /// the memory of _imageData or not
+        /// </summary>
+        protected bool _isShallow = false;
+
         #endregion
 
         #region Properties
         #endregion
 
         #region Methods
+
+        /// <summary>
+        /// Request unmanaged memory for the image data
+        /// </summary>
+        /// <param name="size">The requested memory size in bytes</param>
+        protected void RequestImageData(int size)
+        {
+            DisposeGuard();
+            if (size < 1)
+                throw new ArgumentOutOfRangeException(nameof(size), "Requested memory size has to be 1 or greater");
+            //free old image data if necessary
+            if (_imageData != IntPtr.Zero && !_isShallow)
+                Marshal.FreeHGlobal(_imageData);
+            _imageData = Marshal.AllocHGlobal(size);
+        }
+
         #endregion
 
         #region IDisposable Support
+
+        /// <summary>
+        /// Raises ObjectDisposedException when object has been disposed
+        /// </summary>
+        protected void DisposeGuard()
+        {
+            if (IsDisposed)
+                throw new ObjectDisposedException(nameof(ImageStack));
+        }
 
         /// <summary>
         /// Indicates whether this instance has been disposed
@@ -38,6 +77,11 @@ namespace TwoPAnalyzer.PluginAPI
 
                 // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
                 // TODO: set large fields to null.
+                if (_imageData != IntPtr.Zero && !_isShallow)
+                {
+                    Marshal.FreeHGlobal(_imageData);
+                    _imageData = IntPtr.Zero;
+                }
 
                 IsDisposed = true;
             }
@@ -49,7 +93,9 @@ namespace TwoPAnalyzer.PluginAPI
             Dispose(false);
         }
 
-        
+        /// <summary>
+        /// Dispose object, freeing resources
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
